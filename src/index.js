1d2ff31ab9409c74742bfbd92cf6aca58f781b3d
Abhiram Nair <abhiz123@Abhirams-MacBook-Pro.local>
2020-08-16 22:52:28 +0530

    Added routing to Login, Signup and Dashboard

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import LoginComponent from "./login/login";
+import SignupComponent from "./signup/signup";
+import DashboardComponent from "./dashboard/dashboard";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const firebase = require("firebase");
+require("firebase/firestore");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDhcL65NnZOlCzt5fR5VqNUX9Srl2Wpad0",
+  authDomain: "chatapp-c72b5.firebaseapp.com",
+  databaseURL: "https://chatapp-c72b5.firebaseio.com",
+  projectId: "chatapp-c72b5",
+  storageBucket: "chatapp-c72b5.appspot.com",
+  messagingSenderId: "500887238408",
+  appId: "1:500887238408:web:090b7fe0732b377c78debd",
+  measurementId: "G-91SHJECPWK",
+});
+
+const routing = (
+  <Router>
+    <div id="routing container">
+      <Route path="/login" component={LoginComponent}></Route>
+      <Route path="/signup" component={SignupComponent}></Route>
+      <Route path="/dashboard" component={DashboardComponent}></Route>
+    </div>
+  </Router>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(routing, document.getElementById("root"));
+
 serviceWorker.unregister();
